0bfc087c8583e5a16449671518a2a7dc69972f87
Sam Woodman <sam.woodman@noaa.gov>
2022-07-20 10:59:42 -0700

    add pkgload for deployment

--- app.R
+++ app.R
@@ -3,4 +3,7 @@
 # Or use the blue button on top of this file
 pkgload::load_all(export_all = T, helpers = FALSE, attach_testthat = FALSE)
 
+# install package from github if necessary
+# remotes::install_github("us-amlr/vvipr", ref = "shiny_func", force = TRUE)
+
 vvipr::vvipr()
